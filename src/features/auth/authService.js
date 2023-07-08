3dc89218f0181f52b8f41c550d7235c7cd755389
maryrrr <mary.russu83@gmail.com>
2023-07-08 12:25:13 +0200

    add login

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -7,10 +7,21 @@ const register = async (userData) => {
 
 return res.data;
 
-};
+}
+const login = async(userData)=>{
 
+    const res = await axios.post(API_URL + '/users/login',userData)
+    console.log(res);
+    if (res.data) {
+        
+        localStorage.setItem("user", JSON.stringify(res.data.user));
+        localStorage.setItem("token", JSON.stringify(res.data.token));
+    }
+    return res.data
+}
 const authService = {
     register,
+    login,
 
 };
 
--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -2,13 +2,12 @@ import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
 import authService  from "./authService";
 
 
+const userLocalstorage = JSON.parse(localStorage.getItem("user"));
+const tokenLocalstorage = JSON.parse(localStorage.getItem("token"));
 
 const initialState = {
-
-    user: null,
-
-    token: null,
-
+    user: userLocalstorage ? userLocalstorage:null,
+    token: tokenLocalstorage ? tokenLocalstorage:null
 };
 export const register = createAsyncThunk("auth/register",async (user) => {
     
@@ -19,7 +18,15 @@ export const register = createAsyncThunk("auth/register",async (user) => {
         console.error(error);
         }   
     }
-);
+)
+export const login = createAsyncThunk("auth/login", async (user) => {
+
+    try {
+        return await authService.login(user);
+    } catch (error) {
+    console.error(error);
+    }
+});
 
 export const authSlice = createSlice({
 
